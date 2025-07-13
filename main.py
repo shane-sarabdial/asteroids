ace0fc62ecd292065b6f624fff2e4daf32d4151b
shane <shane@pop-os.mynetworksettings.com>
2025-07-13 16:29:54 -0400

    adding class files

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import pygame
+from pygame import color
+from player import Player
+
 from constants import *
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player1 = Player(x,y)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black")
+        updateable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+        clock.tick(60)
+        dt = clock.tick(60)/1000
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
